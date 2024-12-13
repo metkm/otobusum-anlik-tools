31ad77b2a4bea75111fbb4b21145bece54a78383
metkm <metinkorkmaz417@gmail.com>
2024-12-13 15:04:10 +0300

    ops

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { addLines } from "./addLines";
 import { addLineStops } from "./addLineStops";
 import { getCredentials } from "./requests/getCredentials";
 import { addLineRoutes } from "./addLineRoutes";
+import { addBusLineString } from "./addBusLineString";
 
 console.log('gettings credentials')
 await getCredentials()
@@ -15,3 +16,6 @@ await addLineRoutes()
 
 console.log('adding line stops')
 await addLineStops()
+
+console.log('adding bus lines')
+await addBusLineString()
